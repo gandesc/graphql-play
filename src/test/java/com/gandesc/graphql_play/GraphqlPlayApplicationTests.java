2a07d60c2336313b2115fedd85394d66bc7c2b8f
Mihai Ocneanu <mihai.ocneanu@gmail.com>
2025-08-12 13:16:17 +0300

    initial commit

---
+++ src/test/java/com/gandesc/graphql_play/GraphqlPlayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gandesc.graphql_play;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraphqlPlayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
