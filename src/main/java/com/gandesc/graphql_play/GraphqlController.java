0249d47ab91260fc5f3b8fd8b5643a7e2fb9e2e6
Mihai Ocneanu <mihai.ocneanu@gmail.com>
2025-08-12 13:53:10 +0300

    query with param

--- src/main/java/com/gandesc/graphql_play/GraphqlController.java
+++ src/main/java/com/gandesc/graphql_play/GraphqlController.java
@@ -1,5 +1,6 @@
 package com.gandesc.graphql_play;
 
+import org.springframework.graphql.data.method.annotation.Argument;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
 import org.springframework.stereotype.Controller;
 import reactor.core.publisher.Mono;
@@ -10,5 +11,10 @@ public class GraphqlController {
   @QueryMapping("sayHello")
   public Mono<String> helloWorld() {
     return Mono.just("Hello world");
+  }
+
+  @QueryMapping
+  public Mono<String> sayHelloTo(@Argument String name) {
+    return Mono.fromSupplier(() -> "Hello " + name);
   }
 }
