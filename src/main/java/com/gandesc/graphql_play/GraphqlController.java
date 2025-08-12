4b81df63c670f1eba874ea77585b3c50fd26ec5f
Mihai Ocneanu <mihai.ocneanu@gmail.com>
2025-08-12 13:32:05 +0300

    basic schema & controller

---
+++ src/main/java/com/gandesc/graphql_play/GraphqlController.java
@@ -0,0 +1,14 @@
+package com.gandesc.graphql_play;
+
+import org.springframework.graphql.data.method.annotation.QueryMapping;
+import org.springframework.stereotype.Controller;
+import reactor.core.publisher.Mono;
+
+@Controller
+public class GraphqlController {
+
+  @QueryMapping
+  public Mono<String> sayHello() {
+    return Mono.just("Hello world");
+  }
+}
