36ded27fbae7b35436779f18b6d639f3cdcb6d02
Mihai Ocneanu <mihai.ocneanu@gmail.com>
2025-08-14 12:25:30 +0300

    added controller

---
+++ src/main/java/com/gandesc/graphql_play/sec01/lec02/CustomerController.java
@@ -0,0 +1,31 @@
+package com.gandesc.graphql_play.sec01.lec02;
+
+import com.gandesc.graphql_play.sec01.lec02.dto.Customer;
+import com.gandesc.graphql_play.sec01.lec02.service.CustomerService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.graphql.data.method.annotation.Argument;
+import org.springframework.graphql.data.method.annotation.QueryMapping;
+import org.springframework.stereotype.Controller;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+@Controller
+@RequiredArgsConstructor
+public class CustomerController {
+  private final CustomerService service;
+
+  @QueryMapping
+  public Flux<Customer> customers() {
+    return this.service.allCustomers();
+  }
+
+  @QueryMapping
+  public Mono<Customer> customerById(@Argument Integer id) {
+    return this.service.customerById(id);
+  }
+
+  @QueryMapping
+  public Flux<Customer> customersNameContains(@Argument String name) {
+    return this.service.nameContains(name);
+  }
+}
