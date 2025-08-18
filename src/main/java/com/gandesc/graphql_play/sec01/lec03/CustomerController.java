5cd204178bd7cea02ef760ef63c3d7c8d6f6cfaf
Mihai Ocneanu <mihai.ocneanu@gmail.com>
2025-08-18 12:04:58 +0300

    fetching nested objects

--- src/main/java/com/gandesc/graphql_play/sec01/lec03/CustomerController.java
+++ src/main/java/com/gandesc/graphql_play/sec01/lec03/CustomerController.java
@@ -1,9 +1,12 @@
 package com.gandesc.graphql_play.sec01.lec03;
 
 import com.gandesc.graphql_play.sec01.lec03.dto.Customer;
+import com.gandesc.graphql_play.sec01.lec03.dto.CustomerOrderDto;
 import com.gandesc.graphql_play.sec01.lec03.service.CustomerService;
+import com.gandesc.graphql_play.sec01.lec03.service.OrderService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
+import org.springframework.graphql.data.method.annotation.SchemaMapping;
 import org.springframework.stereotype.Controller;
 import reactor.core.publisher.Flux;
 
@@ -11,10 +14,16 @@ import reactor.core.publisher.Flux;
 @RequiredArgsConstructor
 public class CustomerController {
   private final CustomerService service;
+  private final OrderService orderService;
 
-  @QueryMapping
+  @SchemaMapping(typeName = "Query")
   public Flux<Customer> customers() {
     return this.service.allCustomers();
+  }
+
+  @SchemaMapping(typeName = "Customer")
+  public Flux<CustomerOrderDto> orders(Customer customer) {
+    return this.orderService.ordersByCustomerName(customer.getName());
   }
 
 }
